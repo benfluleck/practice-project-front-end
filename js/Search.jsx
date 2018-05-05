28c0316133b9c97f098ded7057c037b431c1a4a1
Benny Ogidan <andeladeveloper@Andelas-MacBook-Pro-39.local>
2018-05-05 19:39:32 +0100

    chore(displaying tv-show cards): display all tv shows from server - babelrc updated with transform class - updated eslint for jsdocs - installed shortid - implemented ShowCard

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import preload from '../data.json';
+import ShowCard from './ShowCard';
 
-const Search = () => (
-  <h1>This is me</h1>
-);
+
+/**
+ * @class Search
+ *
+ * @extends {Component}
+ */
+export class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+
+  /**
+    * @description- handles change in Input
+    *
+    * @param {event}
+    * @returns {void}
+    * @memberOf Search
+    */
+  handleInputChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    });
+  }
+
+  /**
+  * @description renders Search
+  *
+  * @method render
+  *
+  * @returns {Component} - Search
+  *
+  * @memberOf Search
+  */
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>s-video</h1>
+          <input
+            onChange={this.handleInputChange}
+            type="text"
+            value={this.state.searchTerm}
+            placeholder="Search.." />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default Search;
